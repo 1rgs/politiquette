2c0d03943e3524046c53b3cf03e8bbb6f78bdacb
rahulgs12 <rahul@Rahuls-MacBook-Pro.local>
2017-10-28 18:48:18 -0400

    api, returns average rating for every issue

---
+++ scrape.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+from flask import Flask, request, render_template
+import requests,json
+
+
+def getID(name):
+    r  = requests.get("https://votesmart.org/x/search?s="+"%20".join(name.split()))
+    if "results" in r.json():
+        return [result["candidate_id"] for result in r.json()["results"]][0]
+    return False
+
+def printd(d):
+    return (json.dumps(d, sort_keys=True, indent=4, separators=(',', ': ')))
+
+def getRatings(name):
+
+    ID = getID(name)
+    if not ID:
+        return {"not found":"true"}
+    url = "https://votesmart.org/candidate/evaluations/"+str(ID)+"/"
+    r = requests.get(url)
+    txt = r.text.encode("utf-8")
+    soup = BeautifulSoup(txt, "lxml")
+    issuedivs = soup.findAll("div", {"class":"issues"})
+    allratings = dict()
+    for div in issuedivs:
+        issue = div.h4.text
+        rows = div.table.find_all("tr")
+        allcols = []
+        for row in rows:
+            cols = row.find_all('td')
+            allcols.append([ele.text.strip() for ele in cols])
+        temp = [int(c[2].replace("%","")) for c in allcols if c[2].replace("%","").isdigit()]
+        avg = sum(temp)/len(temp)
+        allratings[issue]=avg
+    return allratings
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def change():
+    status = request.args.get('name', '')
+    ratings = getRatings("Jose Diaz")
+    return printd(ratings)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
