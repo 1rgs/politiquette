2fae3845eda015e128111c2871b75aa672dc3419
williamyeny <williamyeny@gmail.com>
2017-10-29 02:14:17 -0400

    works

--- js/poli-content-script.js
+++ js/poli-content-script.js
@@ -120,10 +120,9 @@ for (i = 0; i < paragraphs.length; i++) {
         console.log(senator + " data: " + data);
       
         var tooltipHtml = "<h2>" + senator + "</h2>";
-        tooltipHtml += "<p>Employment/Affirmative Action: " + data["Employment and Affirmative Action"] + "</p>";
-        tooltipHtml += "<p>Unemployed/Low Income: " + data["Unemployed and Low-Income"] + "</p>";
-        tooltipHtml += "<p>Budget/Spending/Taxes " + data["Budget, Spending and Taxes"] + "</p>";
-        tooltipHtml += "<p>Civil Liberties/Civil Rights: " + data["Civil Liberties and Civil Rights"] + "</p>";
+        tooltipHtml += "<p>Employment/Affirmative Action: " + data["Employment and Affirmative Action"] + "%</p>";
+        tooltipHtml += "<p>Unemployed/Low Income: " + data["Unemployed and Low-Income"] + "%</p>";
+        tooltipHtml += "<p>Civil Liberties/Civil Rights: " + data["Civil Liberties and Civil Rights"] + "%</p>";
         // "Fiscally Conservative", "Socially Conservative", "Socially Liberal", "Fiscally Liberal"
         
         var div = document.createElement("div");
