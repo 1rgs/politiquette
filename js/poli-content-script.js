b00a4202fea998b3a028619109b42a8d1a439657
williamyeny <williamyeny@gmail.com>
2017-10-29 00:56:29 -0400

    shamikh shamikh shamikh shamikh shamikh shamikh shamikh

---
+++ js/poli-content-script.js
@@ -0,0 +1,145 @@
+var paragraphs = document.getElementsByTagName("p");
+
+var senators = [
+  "Richard Shelby",
+  "Luther Strange",
+  "Lisa Murkowski",
+  "Dan Sullivan",
+  "John McCain",
+  "Jeff Flake",
+  "John Boozman",
+  "Tom Cotton",
+  "Dianne Feinstein",
+  "Kamala Harris",
+  "Michael Bennet",
+  "Cory Gardner",
+  "Richard Blumenthal",
+  "Chris Murphy",
+  "Tom Carper",
+  "Chris Coons",
+  "Bill Nelson",
+  "Marco Rubio",
+  "Johnny Isakson",
+  "David Perdue",
+  "Brian Schatz",
+  "Mazie Hirono",
+  "Mike Crapo",
+  "Jim Risch",
+  "Dick Durbin",
+  "Tammy Duckworth",
+  "Joe Donnelly",
+  "Todd Young",
+  "Chuck Grassley",
+  "Joni Ernst",
+  "Pat Roberts",
+  "Jerry Moran",
+  "Mitch McConnell",
+  "Rand Paul",
+  "Bill Cassidy",
+  "John N. Kennedy",
+  "Susan Collins",
+  "Angus King",
+  "Ben Cardin",
+  "Chris Van Hollen",
+  "Elizabeth Warren",
+  "Ed Markey",
+  "Debbie Stabenow",
+  "Gary Peters",
+  "Amy Klobuchar",
+  "Al Franken",
+  "Thad Cochran",
+  "Roger Wicker",
+  "Claire McCaskill",
+  "Roy Blunt",
+  "Jon Tester",
+  "Steve Daines",
+  "Deb Fischer",
+  "Ben Sasse",
+  "Dean Heller",
+  "Catherine Cortez Masto",
+  "Jeanne Shaheen",
+  "Maggie Hassan",
+  "Bob Menendez",
+  "Cory Booker",
+  "Tom Udall",
+  "Martin Heinrich",
+  "Chuck Schumer",
+  "Kirsten Gillibrand",
+  "Richard Burr",
+  "Thom Tillis",
+  "John Hoeven",
+  "Heidi Heitkamp",
+  "Sherrod Brown",
+  "Rob Portman",
+  "Jim Inhofe",
+  "James Lankford",
+  "Ron Wyden",
+  "Jeff Merkley",
+  "Bob Casey Jr.",
+  "Pat Toomey",
+  "Jack Reed",
+  "Sheldon Whitehouse",
+  "Lindsey Graham",
+  "Tim Scott",
+  "John Thune",
+  "Mike Rounds",
+  "Lamar Alexander",
+  "Bob Corker",
+  "John Cornyn",
+  "Ted Cruz",
+  "Orrin Hatch",
+  "Mike Lee",
+  "Patrick Leahy",
+  "Bernie Sanders",
+  "Mark Warner",
+  "Tim Kaine",
+  "Patty Murray",
+  "Maria Cantwell",
+  "Joe Manchin",
+  "Shelley Moore Capito",
+  "Ron Johnson",
+  "Tammy Baldwin",
+  "Mike Enzi",
+  "John Barrasso",
+]
+for (i = 0; i < paragraphs.length; i++) {
+  var p = paragraphs[i];
+
+  for (j = 0; j < senators.length; j++) {
+    if (p.innerHTML.includes(senators[j])) {
+      var urlName = senators[j].split(" ").join("%20");
+      chrome.runtime.sendMessage({
+        method: 'GET',
+        action: 'xhttp',
+        url: 'https://politiquette.herokuapp.com/?name=' + urlName,
+        senator: senators[j]
+      }, function(responseText) {
+        var response = JSON.parse(responseText);
+        var senator = Object.keys(response)[0];
+        var data = response[senator];
+        console.log(senator + " data: " + data);
+      
+        var tooltipHtml = "<h2>" + senator + "</h2>";
+        tooltipHtml += "<p>Employment/Affirmative Action: " + data["Employment and Affirmative Action"] + "</p>";
+        tooltipHtml += "<p>Unemployed/Low Income: " + data["Unemployed and Low-Income"] + "</p>";
+        tooltipHtml += "<p>Budget/Spending/Taxes " + data["Budget, Spending and Taxes"] + "</p>";
+        tooltipHtml += "<p>Civil Liberties/Civil Rights: " + data["Civil Liberties and Civil Rights"] + "</p>";
+        // "Fiscally Conservative", "Socially Conservative", "Socially Liberal", "Fiscally Liberal"
+        
+        var div = document.createElement("div");
+        div.setAttribute("class","poli-tooltip");
+        div.innerHTML = tooltipHtml;
+        var senatorHtml = document.querySelectorAll("[senator=\"" + senator + "\"]")[0];
+        senatorHtml.appendChild(div);
+        senatorHtml.removeAttribute("senator");
+
+        
+        console.log(data);
+      });
+      p.innerHTML = p.innerHTML.split(senators[j]).join("<span class=\"poli-hover\" senator=\"" + senators[j] +"\">" + senators[j] + "</span>");
+      
+      console.log("found senator " + senators[j] + ", sending...");
+    }
+  }
+
+}
